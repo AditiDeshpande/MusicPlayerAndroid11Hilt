4c8327cf23d43844969835becd8e7ad5d9e7edb6
aditi <aditid11@gmail.com>
2020-09-12 15:55:38 +0530

    Just following Android Developer's video on Hilt. It's just a skeleton. It needs code implementation and a lot of refactoring. Package structure is also not in place. Will be working on it later.

--- app/src/androidTest/java/com/example/musicplayer/MusicPlayerTestWithHilt.kt
+++ app/src/androidTest/java/com/example/musicplayer/MusicPlayerTestWithHilt.kt
@@ -1,9 +1,16 @@
 package com.example.musicplayer
 
+import android.app.Application
+import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import junit.framework.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
@@ -12,6 +19,7 @@ import org.junit.runner.RunWith
 import javax.inject.Inject
 
 @HiltAndroidTest
+@UninstallModules(DataModule::class)
 @RunWith(AndroidJUnit4::class)
 class MusicPlayerTestWithHilt {
 
@@ -31,5 +39,14 @@ class MusicPlayerTestWithHilt {
     fun testPlay(){
         player.play("Asha Bhosale")
         assertTrue(player.isPlaying())
+    }
+
+    @Module
+    @InstallIn(ApplicationComponent::class)
+    object TestModule {
+        @Provides
+        fun provideDB(app: Application) =
+            Room.inMemoryDatabaseBuilder(app ,
+            MusicDatabase::class.java).build()
     }
 }
